647f2f5c41aed7cc210eea4ebba1396ec2681189
Denis Levy <denis.levi11@gmail.com>
2021-03-02 19:27:44 -0300

    Criada a classe Position

---
+++ Xadrez/Tabuleiro/Position.cs
@@ -0,0 +1,15 @@
+﻿namespace Xadrez.Tabuleiro {
+    class Position {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public Position(int row, int column) {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString() {
+            return $"({Row}, {Column})";
+        }
+    }
+}
